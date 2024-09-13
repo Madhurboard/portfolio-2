31b4373eea41e4d4cee8deeefe285a8439f7b60d
madhurpatil73 <madhurpatil73@gmail.com>
2024-09-13 15:35:22 +0000

    Completed With all the sections of the project.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
               <div className="w-full lg:w-1/2">
                   <div className="flex flex-col items-center lg:items-start">
                       <h1 className="  ml-4 pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">Madhur Patil</h1>
-                      <span className=" ml-4 bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">Full Stack Developer</span>
+                      <span className=" ml-4 bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">Full Stack Developer</span>
                       <p className=" ml-4 my-2 max-w-xl py-6 font-light tracking-tighter">{HERO_CONTENT}</p>
                   </div>
               </div>
